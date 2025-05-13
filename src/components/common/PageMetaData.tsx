2381195dc141501097d8a8dcd9bc8d9a64358ecb
Dave <uchennadavid2404@gmail.com>
2025-05-13 03:15:55 +0100

    open graph correct

--- src/components/common/PageMetaData.tsx
+++ src/components/common/PageMetaData.tsx
@@ -15,12 +15,19 @@ export function generatePageMetadata({ title, content, image, url }: MetaDataPro
 		description: content,
 		openGraph: {
 			type: 'website',
-			siteName: '100 Minds dashboard',
+			siteName: 'Millennia Trades dashboard',
 			locale: 'en_US',
 			title: `${title}`,
 			description: content,
 			url: url || 'https://admin.millenniatrades.com',
-			images: ['opengraph-image.png'],
+			images: [
+				{
+					url: 'https://www.admin.millenniatrades.com/opengraph-image.png',
+					width: 1200,
+					height: 630,
+					alt: 'Millennia Trades Open Graph Image',
+				},
+			],
 		},
 		twitter: {
 			card: image || defaultImage ? 'summary_large_image' : 'summary',
@@ -31,6 +38,6 @@ export function generatePageMetadata({ title, content, image, url }: MetaDataPro
 		other: {
 			'og:locale': 'en_US',
 		},
-		metadataBase: new URL('https://admin.millenniatrades.com'),
+		metadataBase: new URL('https://www.admin.millenniatrades.com'),
 	};
 }
